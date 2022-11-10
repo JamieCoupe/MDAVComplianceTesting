1a3e0f91ec4df387fb438a613e032e7e1c0c2cc0
Jamie-Coupe <jamie.coupe@outlook.com>
2022-11-10 23:29:25 +0000

    Added test files

---
+++ test/public/Set-MDAVConfig.Tests.ps1
@@ -0,0 +1,23 @@
+. .\Import.ps1
+
+BeforeTest
+
+Describe $module -Tags ('unit') {
+    Context "$module : Module Tests" { 
+        Context "Parameter Tests" { 
+            Context "Exist" { 
+
+            } 
+            
+            Context "Mandatory if required" { 
+                
+            }
+
+            Context "Validate Set is correct" { 
+
+            }
+        }
+    }
+}
+
+AfterTest
